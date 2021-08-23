a87ad65bc090bd5e2055bebc7707eb6866be988b
andrea <kjkandrea@gmail.com>
2021-08-23 22:16:38 +0900

    Add my cute doggy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+
+app.get("/", (req, res) => res.render("home"))
 
 const handleListen = () => console.log('app listen : %s', `http://localhost:${port}`)
 app.listen(port, handleListen)
