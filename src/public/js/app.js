dc06441e915ff899a62d8535cae0cdb090d5b186
andrea <kjkandrea@gmail.com>
2021-10-04 18:29:54 +0900

    Add room dom

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,11 @@ const socket = io()
 
 const welcomeEl = document.getElementById('welcome')
 const welcomeFormEl = welcomeEl.querySelector('form')
+const roomEl = document.getElementById('room')
+
+document.addEventListener('DOMContentLoaded', () => {
+  roomEl.hidden = true
+})
 
 const handleWelcomeSubmit = evt => {
   evt.preventDefault();
