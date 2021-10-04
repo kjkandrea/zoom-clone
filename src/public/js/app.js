5d820c49f3d0b8afe6940c8f81a768f107c9006b
andrea <kjkandrea@gmail.com>
2021-10-04 17:47:18 +0900

    Add hd sm evt

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
 const socket = io()
+
+const welcomeEl = document.getElementById('welcome')
+const welcomeFormEl = welcomeEl.querySelector('form')
+
+const handleWelcomeSubmit = evt => {
+  evt.preventDefault();
+  const inputEl = evt.target.querySelector('input')
+  console.log(inputEl.value)
+  inputEl.value = ''
+}
+
+welcomeFormEl.addEventListener('submit', handleWelcomeSubmit)
