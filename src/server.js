cb3d329501d31524a90e46138dc548febee2eb14
andrea <kjkandrea@gmail.com>
2021-10-16 18:18:46 +0900

    Remove legacy code

--- src/server.js
+++ src/server.js
@@ -50,28 +50,4 @@ io.on('connection', socket => {
   socket.on('nickname', nickname => socket.nickname = nickname)
 })
 
-// const wss = new WebSocket.Server({ server })
-
-// const sockets = []
-//
-// const parseMessagePayload = msg => msg.toString('utf8')
-// const handleMessage = (msg, socket) => {
-//   const { type, payload } = JSON.parse(msg)
-//   if (type === "message") sockets.forEach(s =>
-//     s.send(JSON.stringify({
-//       nickname: socket.nickname,
-//       message: parseMessagePayload(payload)
-//     })))
-//   if (type === "nickname") socket['nickname'] = payload
-// }
-//
-// const handleConnection = socket => {
-//   sockets.push(socket)
-//   console.log('🧚 WebSocket Connected to Client.')
-//   socket['nickname'] = 'anonymous'
-//
-//   socket.on('close', ()=> console.log('🧚 WebSocket Disconnected from
-// Client.')) socket.on('message', msg => handleMessage(msg, socket)) }
-// wss.on("connection", handleConnection)
-
 server.listen(3000, handleListen)
